9931e727b2b2f5a8d742f6a700848578ba507642
KiwiSorbet <chrisghosn@gmail.com>
2024-07-09 20:58:17 -0400

    initial commit

---
+++ C/main.c
@@ -0,0 +1,14 @@
+#include <ctype.h>
+#include <float.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int main() {
+    
+}
