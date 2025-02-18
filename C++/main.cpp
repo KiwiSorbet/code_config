07a430c90e53c71da9b817ff838ffd3072c28f9e
KiwiSorbet <galaxygum51@gmail.com>
2025-02-17 20:13:40 -0500

    reverted main.cpp changes

--- C++/main.cpp
+++ C++/main.cpp
@@ -1,41 +1,5 @@
-#include <iomanip>
 #include <iostream>
 
-int firstSum(int);
-int triangle(int);
-
-int main() {
-    int inputnumber;
-    std::cout << "Enter a positive number: ";
-    std::cin >> inputnumber;
-
-    if (inputnumber >= 0)
-        triangle(inputnumber);
-    else
-        main();
-    return 0;
-}
-
-int firstSum(int input) {
-    int x = input;
-    int y = input;
-    int sum;
-    for (int i = 0; i < x; i++) {
-        sum = 0;
-        for (int j = 1; j <= y; j++)
-            sum = sum + j * 3;
-        std::cout << sum << std::setw(5);
-        y = y - 1;
-    }
-    std::cout << "\n";
-    return 0;
-}
-
-int triangle(int inputnumber) {
-    int i = inputnumber;
-    while (i > 0) {
-        firstSum(i);
-        i = i - 1;
-    }
-    return 0;
+int main(int argc, char* argv[]) {
+    
 }
