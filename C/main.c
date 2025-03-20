df5f623d571ffdae4047abddd6fca44935a7b73c
KiwiSorbet <galaxygum51@gmail.com>
2025-03-20 05:43:45 -0400

    fixed disabling unused includes

--- C/main.c
+++ C/main.c
@@ -1,14 +1,14 @@
-#include <ctype.h>
-#include <float.h>
-#include <limits.h>
-#include <math.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <time.h>
+#include <ctype.h> // IWYU pragma: keep
+#include <float.h> // IWYU pragma: keep
+#include <limits.h> // IWYU pragma: keep
+#include <math.h> // IWYU pragma: keep
+#include <stdbool.h> // IWYU pragma: keep
+#include <stdint.h> // IWYU pragma: keep
+#include <stdio.h> // IWYU pragma: keep
+#include <stdlib.h> // IWYU pragma: keep
+#include <string.h> // IWYU pragma: keep
+#include <time.h> // IWYU pragma: keep
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     
 }
