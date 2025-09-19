287fb94f8af0c92ae760375609589ef91b532248
KiwiSorbet <galaxygum51@gmail.com>
2025-09-18 20:40:12 -0400

    Web: remove custom port for dev mode

--- Web/vite.config.ts
+++ Web/vite.config.ts
@@ -2,5 +2,5 @@ import {defineConfig} from "vite";
 
 export default defineConfig({
     build: {outDir: "build", emptyOutDir: true},
-    server: {port: 4000, open: true},
+    server: {open: true},
 });
